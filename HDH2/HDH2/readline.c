e8886afc7e9c25d3d1709a26b5d37fd0dffb0749
khuongvo2305 <44772083+khuongvo2305@users.noreply.github.com>
2019-02-22 23:17:55 +0700

    Update readline.c

--- HDH2/HDH2/readline.c
+++ HDH2/HDH2/readline.c
@@ -1,27 +1,31 @@
 #include "readline.h"
-int readline(FILE *fp, char *buffer, int address)
+//int readline(FILE *fp, char *buffer, int address)
+char* readline(FILE *fp, char *buffer, int buff_len)
 {
     int ch;
     int i = 0;
-    size_t buff_len = 0;
     
-    //buffer = malloc(buff_len + 1);
-    if (address == 0) buffer = malloc(buff_len + 1);
-    else buffer = realloc(buffer, address + 1);
-    if (!buffer) return 0;  // Out of memory
+    buffer = malloc(buff_len + 1);
+    //if (address == 0) buffer = malloc(buff_len + 1);
+    //else buffer = realloc(buffer, address + 1);
+    if (!buffer) return NULL;  // Out of memory
     
     while ((ch = fgetc(fp)) != '\n' && ch != EOF)
     {
         buff_len++;
-        void *tmp = realloc(buffer, address + buff_len + 1);
+        //void *tmp = realloc(buffer, address + buff_len + 1);
+        void *tmp = realloc(buffer, buff_len + 1);
+
         if (tmp == NULL)
         {
             free(buffer);
-            return 0; // Out of memory
+            return NULL; // Out of memory
         }
         buffer = tmp;
         
-        buffer[address + i] = (char) ch;
+        buffer[i] = (char) ch;
+        //buffer[address + i] = (char) ch;
+
         i++;
     }
     buffer[i] = '\0';
@@ -31,15 +35,18 @@ int readline(FILE *fp, char *buffer, int address)
     if (ch == EOF && (i == 0 || ferror(fp)))
     {
         free(buffer);
-        return 0;
+        return NULL;
     }
-    address+=buff_len;
-    return buff_len;
+    //address+=buff_len;
+    return buffer;
 }
 int read_line(char *str)
 {
     FILE* fp;
-    int address = 0;
+    int buff_len = 0;
+    //int address = 0;
     fp = fopen("/Users/khuong/Documents/HDH2/HDH2/input.txt","r");
-    return readline(fp, str, &address);
+    //return readline(fp, str, &address);
+    str = readline(fp,str,&buff_len);
+    return buff_len;
 }
